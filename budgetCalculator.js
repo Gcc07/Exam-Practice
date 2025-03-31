0decb0c034ad7e894b8b040be609f68c1bc2f403
SirGuamsalot <gcardenasnc@icloud.com>
2025-03-31 13:26:23 -0400

    Completed Assignment

---
+++ budgetCalculator.js
@@ -0,0 +1,32 @@
+//Use the .toFixed(2) method to round the calculated values to two decimal places.
+
+button = document.getElementById('calculateBudget');
+monthlyIncome = document.getElementById('monthlyIncome');
+monthlyExpenses = document.getElementById('estimatedExpenses');
+monthsProjected = document.getElementById('monthsProjected');
+savings = document.getElementById('savingsResult');
+
+button.addEventListener('click', function() {
+    let income = monthlyIncome.value;
+    let expenses = monthlyExpenses.value;
+    let money_saved = calculateBudget(income, expenses);
+    console.log(money_saved);
+    if (money_saved <= 0) {
+        alert("You are spending more than you are earning, or earning nothing. Please review your budget.");
+    } else {
+        savings.textContent = "Monthly Savings: $"+ money_saved;
+
+        for (let i = 1; i <= monthsProjected.value; i++) {
+            
+            let newSavings = money_saved * i; // Multiplies the savings by # of months
+            let newElement = document.createElement('li');
+            newElement.innerHTML = `In ${i} months, you will have saved $${newSavings}`;
+            savings.appendChild(newElement);
+        }
+    }
+});
+
+function calculateBudget(income, expenses) {
+    let Savings = income - expenses;
+    return Savings.toFixed(2);
+}
